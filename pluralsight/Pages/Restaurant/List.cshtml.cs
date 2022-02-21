e66d9467611c76ab5ff0ad1d5f47e294df097af1
richie <richie.kwon1@gmail.com>
2022-02-21 20:30:33 +0000

    restful

--- pluralsight/Pages/Restaurant/List.cshtml.cs
+++ pluralsight/Pages/Restaurant/List.cshtml.cs
@@ -1,22 +1,28 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OfoodData;
 
 namespace pluralsight.Pages.Restaurant
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaunrantData;
 
         // public int MyProperty { get; set; }
         public string Message { get; set; }
+        public IEnumerable<Ofood.Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
+            this.config = config;
         }
 
-        public void OnGet()
+        public void OnGet()  
         {
             Message = "Hello, World!";
+            Restaurants = restaunrantData.GetAll();
         }
     }
-}
+}  
